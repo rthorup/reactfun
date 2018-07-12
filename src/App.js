0c537935d2442fcecbe31b2b46c643c4668a4c34
rthorup <ryan.thorup@gmail.com>
2018-07-12 15:59:41 -0500

    created a list render with vote up/vote down/add/delete to test changing state and passing thing through methods.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,113 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var shortid = require('shortid');
+
+function User(props) {
+  return(
+  <li className="item">
+   Name: {props.name}
+   <button onClick={props.voteUp}>Vote UP</button>
+   <button onClick={props.voteDown}>Vote DOWN</button>
+    <button onClick={props.delEvent}>Delete</button>
+  </li>
+  )
+}
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        {name: 'Ryan',  key: shortid.generate()},
+        {name: 'Taylor', key: shortid.generate()},
+        {name: 'Andy', key: shortid.generate()},
+        {name: 'Dru', key: shortid.generate()},
+        {name: 'Robert', key: shortid.generate()}
+      ],
+      newName: ''
+    }
+  }
+  updateName(e) {
+    this.setState({newName: e.target.value});
+  }
+
+  addUser(e) {
+    let newUser = { name: this.state.newName, key: shortid.generate()}
+    let userList = this.state.users;;
+    userList.push(newUser);
+    this.setState({users: userList})
+    this.setState({newName: ''})
+  }
+
+  vote (key, message, e) {
+    let newUsers = this.state.users;
+    let placeholder = newUsers.findIndex((user) => {
+      return user.key === key;
+    })
+    if (message === true && placeholder === 0) {
+      alert(this.state.users[placeholder].name + ' is already the best!')
+    } else if (message === false && placeholder === newUsers.length -1) {
+      alert(this.state.users[placeholder].name + ' is already the biggest loser!')
+    }
+     else {
+       let plusMinus = placeholder;
+       console.log(plusMinus);
+       if(message === true) {
+         console.log(true);
+         plusMinus--;
+
+       }
+       else if(message === false){
+         console.log(false);
+         plusMinus++;
+
+       }
+    console.log(plusMinus);
+    let movedItem = newUsers.splice(placeholder, 1)[0];
+    newUsers.splice(plusMinus, 0, movedItem);
+    console.log(newUsers);
+    this.setState({users: newUsers});
+  }
+}
+
+
+
+  // voteDown (key, e) {
+  //   let newUsers = this.state.users;
+  //   let placeholder = newUsers.findIndex((user) => {
+  //     return user.key === key;
+  //   })
+  //   if(placeholder !== newUsers.length -1) {
+  //   let movedItem = newUsers.splice(placeholder, 1)[0];
+  //   newUsers.splice(placeholder += 1, 0, movedItem);
+  //   this.setState({users: newUsers});
+  // } else {
+  //   alert(this.state.users[placeholder].name + ' is already the biggest loser!')
+  // }
+  // }
+
+  deleteUser(index, key, e) {
+    let newUsers = this.state.users;
+    newUsers.splice(index, 1);
+    this.setState({users: newUsers})
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello my friend!</h1>
+      <div className="mainDiv">
+        <ul>
+          {
+            this.state.users.map((user, index) => {
+              return <User key={user.key} name = {user.name} voteUp={this.vote.bind(this, user.key, true)} voteDown={this.vote.bind(this, user.key, false)}  delEvent= {this.deleteUser.bind(this, index, user.key)}/>
+            })
+          }
+
+        </ul>
+        <div className="item">
+          <label>Add new user:  </label>
+          <input onChange={this.updateName.bind(this)} value={this.state.newName}></input>
+          <button onClick={this.addUser.bind(this)}>Add user</button>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,56 +1,142 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-    this.stopTime = this.stopTime.bind(this);
-  }
+// var shortid = require('shortid');
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      this.props.time
-  )}
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {date: new Date()};
+//     this.stopTime = this.stopTime.bind(this);
+//   }
+//
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.tick(),
+//       this.props.time
+//   )}
+//
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+//
+//   stopTime(){
+//     this.componentWillUnmount();
+//   }
+//
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//         <button onClick = {this.stopTime}>
+//           Stop time!
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+//
+// <h1>"my name is " + mVar +  " nice to you"</h1>
+// function App ()  {
+//   return (
+//     <div>
+//       <Clock time = "1000"/>
+//       <Clock time = "5000"/>
+//     </div>
+//   )
+// }
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
 
-  stopTime() {
-    clearInterval(this.timerID);
-  }
 
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        <button onClick = {this.stopTime}>
-          Stop time!
-        </button>
-      </div>
-    );
-  }
-}
+// function UserGreeting(props) {
+//   return <h1>Welcome back!</h1>;
+// }
+//
+// function GuestGreeting(props) {
+//   return <h1>Please sign up.</h1>;
+// }
+//
+// function Greeting(props) {
+//   const isLoggedIn = props.isLoggedIn;
+//   if (isLoggedIn) {
+//     return <UserGreeting />;
+//   }
+//   return <GuestGreeting />;
+// }
+//
+// function LoginButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Login
+//     </button>
+//   );
+// }
+//
+// function LogoutButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Logout
+//     </button>
+//   );
+// }
+//
+// class LoginControl extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleLoginClick = this.handleLoginClick.bind(this);
+//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//     this.state = {isLoggedIn: false};
+//   }
+//
+//   handleLoginClick() {
+//     this.setState({isLoggedIn: true});
+//   }
+//
+//   handleLogoutClick() {
+//     this.setState({isLoggedIn: false});
+//   }
+//
+//   render() {
+//     const isLoggedIn = this.state.isLoggedIn;
+//     let button;
+//
+//     if (isLoggedIn) {
+//       button = <LogoutButton onClick={this.handleLogoutClick} />;
+//     } else {
+//       button = <LoginButton onClick={this.handleLoginClick} />
+//     }
+//
+//     return (
+//       <div>
+//         <Greeting isLoggedIn={isLoggedIn} />
+//         {button}
+//       </div>
+//     );
+//   }
+// }
+//
 
-function App() {
-  return (
-    <div>
-      <Clock time = "1000" />
-      <Clock time = "5000" />
-      <Clock time = "10000" />
-    </div>
-  );
-}
+
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+
 
 registerServiceWorker();
